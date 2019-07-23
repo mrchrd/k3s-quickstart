b80084a87d0ad7ad4621c203bcdde1b97daf3ea6
Michael Richard <michael.richard.ing@gmail.com>
2019-07-23 12:19:20 -0400

    Clean output

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ START_TIME=`date "+%s"`
 
 echo '### Setup k3s'
 docker-compose up -d
-${EXEC} 'until kubectl get nodes; do sleep 1; done'
-${EXEC} 'until [ `echo $(kubectl get deployment -n kube-system coredns -o jsonpath='{.status.readyReplicas}' --ignore-not-found) | sed 's/^$/0/'` -gt 0 ]; do sleep 1; done'
+${EXEC} 'until kubectl wait --for=condition=ready node/$(hostname); do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/coredns -n kube-system; do sleep 1; done' 2>/dev/null
 echo
 
 echo "### Setup Metrics Server"
@@ -17,30 +17,37 @@ echo
 
 echo "### Setup OpenEBS"
 ${EXEC} 'kubectl apply -f-' < manifests/openebs.yaml
-${EXEC} 'until kubectl get storageclass openebs-hostpath -o name; do sleep 1; done'
+${EXEC} 'until kubectl wait --for=condition=available deployment/openebs-admission-server -n openebs; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/openebs-apiserver -n openebs; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/openebs-localpv-provisioner -n openebs; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/openebs-ndm-operator -n openebs; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/openebs-provisioner -n openebs; do sleep 1; done' 2>/dev/null
 ${EXEC} "kubectl patch storageclass openebs-hostpath -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}'"
 echo
 
 echo "### Setup Istio"
 ${EXEC} 'kubectl apply -f-' < manifests/istio-init.yaml
-${EXEC} 'until kubectl get crd rbacconfigs.rbac.istio.io -o name; do sleep 1; done'
+${EXEC} 'until kubectl wait --for=condition=complete job/istio-init-crd-10 -n istio-system; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=complete job/istio-init-crd-11 -n istio-system; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=complete job/istio-init-crd-12 -n istio-system; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=complete job/istio-init-crd-certmanager-10 -n istio-system; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=complete job/istio-init-crd-certmanager-11 -n istio-system; do sleep 1; done' 2>/dev/null
 ${EXEC} 'kubectl apply -f-' < manifests/istio.yaml
-${EXEC} 'until kubectl get crd rbacconfigs.rbac.istio.io -o name; do sleep 1; done'
 ${EXEC} 'kubectl apply -f-' < manifests/cert-manager-issuers.yaml
-${EXEC} 'until [ `echo $(kubectl get deployment -n istio-system istio-pilot -o jsonpath='{.status.readyReplicas}' --ignore-not-found) | sed 's/^$/0/'` -gt 0 ]; do sleep 1; done'
-${EXEC} 'until [ `echo $(kubectl get deployment -n istio-system istio-policy -o jsonpath='{.status.readyReplicas}' --ignore-not-found) | sed 's/^$/0/'` -gt 0 ]; do sleep 1; done'
-${EXEC} 'until [ `echo $(kubectl get deployment -n istio-system istio-sidecar-injector -o jsonpath='{.status.readyReplicas}' --ignore-not-found) | sed 's/^$/0/'` -gt 0 ]; do sleep 1; done'
+${EXEC} 'until kubectl wait --for=condition=available deployment/istio-pilot -n istio-system; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/istio-policy -n istio-system; do sleep 1; done' 2>/dev/null
+${EXEC} 'until kubectl wait --for=condition=available deployment/istio-sidecar-injector -n istio-system; do sleep 1; done' 2>/dev/null
 echo
 
 echo "### Setup NGINX"
 ${EXEC} 'kubectl apply -f-' < manifests/nginx.yaml
-${EXEC} 'until [ `echo $(kubectl get deployment -n test nginx -o jsonpath='{.status.readyReplicas}' --ignore-not-found) | sed 's/^$/0/'` -gt 0 ]; do sleep 1; done'
+${EXEC} 'until kubectl wait --for=condition=available deployment/nginx -n test; do sleep 1; done' 2>/dev/null
 ${EXEC} 'kubectl exec -i -n test $(kubectl get pod -n test -l app=nginx -o name | sed "s|pod/||") -c nginx -- sh -c "cat > /usr/share/nginx/html/index.htm"' < assets/index.html
 echo
 
 END_TIME=`date "+%s"`
 
-if which curl; then
+if which curl >/dev/null; then
   echo "### Test http (expect 301)"
   curl http://nginx.test --connect-to nginx.test:80:localhost:80 -I
   echo
